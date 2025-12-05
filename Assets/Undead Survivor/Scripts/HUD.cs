94c8b6f041cf7fd15d13a62c697dfc281edf47c5
y <shootfruit@naver.com>
2025-12-05 23:20:21 +0900

    레벨, 킬 수 HUD 추가

---
+++ Assets/Undead Survivor/Scripts/HUD.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoType { Exp, Level, Kill, Time, Health }
+    public InfoType type;
+
+    Text myText;
+    Slider mySlider;
+
+    private void Awake()
+    {
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
+    }
+
+    private void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+                float curExp = GameManager.instance.exp;
+                float nextExp = GameManager.instance.nextExp[GameManager.instance.level];
+                mySlider.value = curExp / nextExp;
+                break;
+            case InfoType.Level:
+                myText.text = string.Format("Lv.{0:F0}", GameManager.instance.level);
+                break;
+            case InfoType.Kill:
+                myText.text = string.Format("{0:F0}", GameManager.instance.kill);
+                break;
+            case InfoType.Time:
+               
+                break;
+            case InfoType.Health:
+
+                break;
+        }
+    }
+}
