7ca456c84859ba671be0ef979ac4fe561a3d01d4
y <shootfruit@naver.com>
2025-12-06 00:28:54 +0900

    아이템1~5 레이아웃 구성

---
+++ Assets/Undead Survivor/Scripts/Item.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public ItemData data;
+    public int level;
+    public Weapon weapon;
+
+    Image icon;
+    Text textLevel;
+
+    private void Awake()
+    {
+        icon = GetComponentsInChildren<Image>()[1];
+        icon.sprite = data.itemIcon;
+
+        Text[] texts = GetComponentsInChildren<Text>();
+        textLevel = texts[0];
+
+    }
+
+    private void LateUpdate()
+    {
+        textLevel.text = "Lv." + (level + 1);
+    }
+}
